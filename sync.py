b34ae0b42cf9e1f167cf9c9ccb4f3fa721bb365f
Konrad Michalik <konrad.michalik@xima.de>
2020-04-14 09:13:10 +0200

    [Improvement] Colored console output

--- sync.py
+++ sync.py
@@ -32,9 +32,9 @@ ignore_database_tables = [
 def main():
     global config
 
-    print('###############################')
-    print('#     TYPO3 Database Sync     #')
-    print('###############################')
+    print(bcolors.BLACK + '###############################' + bcolors.ENDC)
+    print(bcolors.BLACK + '#     TYPO3 Database Sync     #' + bcolors.ENDC)
+    print(bcolors.BLACK + '###############################' + bcolors.ENDC)
 
     check_configuration()
     create_remote_database_dump()
@@ -43,7 +43,7 @@ def main():
     clean_up()
 
     ssh_client.close()
-    print('[System] Successfully synchronized databases')
+    _print(subject.INFO, 'Successfully synchronized databases', True)
 
 #
 # CHECK CONFIGURATION
@@ -62,9 +62,9 @@ def get_host_configuration():
     if os.path.isfile('host.json'):
         with open('host.json', 'r') as read_file:
             config['host'] = json.load(read_file)
-            print('[Local] Load host configuration')
+            _print(subject.LOCAL, 'Loading host configuration', True)
     else:
-        sys.exit('[Error] Local host configuration not found')
+        sys.exit(_print(subject.ERROR, 'Local host configuration not found', False))
 
 def load_pip_modules():
     import importlib
@@ -73,9 +73,9 @@ def load_pip_modules():
     try:
         import pip
     except ImportError:
-        sys.exit('[Error] Pip is not installed')
+        sys.exit(_print(subject.ERROR, 'Pip is not installed', False))
 
-    print('[Local] Load pip modules')
+    _print(subject.LOCAL, 'Loading pip modules', True)
     package = 'paramiko'
     try:
         importlib.import_module(package)
@@ -86,12 +86,12 @@ def load_pip_modules():
 
 def get_remote_password():
     global remote_ssh_password
-    _password = getpass.getpass('[System] SSH password for user "' + config['host']['remote']['user'] + '": ')
+    _password = getpass.getpass(_print(subject.INFO, 'SSH password for user "' + config['host']['remote']['user'] + '": ', False))
     remote_ssh_password = _password
 
 def check_local_configuration():
     if os.path.isfile(config['host']['local']['path']) == False:
-        sys.exit('[Error] Local database configuration not found')
+        sys.exit(_print(subject.ERROR, 'Local database configuration not found', False))
 
     config['db'] = {}
     #
@@ -99,7 +99,7 @@ def check_local_configuration():
     #
     _local_db_config = check_output(['php', '-r', 'echo json_encode(include "' + config['host']['local']['path'] + '");'])
     _local_db_config = json.loads(_local_db_config)['DB']['Connections']['Default']
-    print('[Local] Checking database configuration')
+    _print(subject.LOCAL, 'Checking database configuration', True)
     config['db']['local'] = _local_db_config
 
 def check_remote_configuration():
@@ -107,7 +107,7 @@ def check_remote_configuration():
     _remote_db_config = stdout.readlines()[0]
     _remote_db_config = json.loads(_remote_db_config)['DB']['Connections']['Default']
     config['db']['remote'] = _remote_db_config
-    print('[Remote] Checking database configuration')
+    _print(subject.REMOTE, 'Checking database configuration', True)
 
 #
 # CREATE REMOTE DATABASE DUMP
@@ -115,12 +115,12 @@ def check_remote_configuration():
 def create_remote_database_dump():
     generate_database_dump_filename()
 
-    print('[Remote] Creating database dump')
+    _print(subject.REMOTE, 'Creating database dump', True)
     run_ssh_command('mysqldump -u' + config['db']['remote']['user'] + ' -p' + config['db']['remote']['password'] + ' ' + config['db']['remote']['dbname'] + ' --ignore-table=' + generate_ignore_database_tables() + ' > ~/' + remote_database_dump_file_name)
     prepare_remote_database_dump()
 
 def prepare_remote_database_dump():
-    print('[Remote] Prepare database dump')
+    _print(subject.REMOTE, 'Preparing database dump', True)
     run_ssh_command('tar cfvz ~/' + remote_database_dump_file_name + '.tar.gz ' + remote_database_dump_file_name)
 
 def generate_database_dump_filename():
@@ -142,7 +142,7 @@ def get_remote_database_dump():
     create_temporary_data_dir()
 
     sftp = ssh_client.open_sftp()
-    print('[Remote] Download database dump')
+    _print(subject.REMOTE, 'Downloading database dump', True)
     #
     # ToDo: Download speed problems
     # https://github.com/paramiko/paramiko/issues/60
@@ -162,11 +162,11 @@ def import_database_dump():
     prepare_local_database_dump()
     check_local_database_dump()
 
-    print('[Local] Import database dump')
+    _print(subject.LOCAL, 'Importing database dump', True)
     os.system('mysql -u' + config['db']['local']['user'] + ' -p' + config['db']['local']['password'] + ' ' + config['db']['local']['dbname'] + ' < ' + os.path.abspath(os.getcwd()) + '/.sync/' + remote_database_dump_file_name)
 
 def prepare_local_database_dump():
-    print('[Local] Prepare database dump')
+    _print(subject.LOCAL, 'Preparing database dump', True)
     os.system('tar xzf ' + os.path.abspath(os.getcwd()) + '/.sync/' + remote_database_dump_file_name + '.tar.gz')
     os.system('mv ' + os.path.abspath(os.getcwd()) + '/' + remote_database_dump_file_name + ' ' + os.path.abspath(os.getcwd()) + '/.sync/' + remote_database_dump_file_name)
 
@@ -174,7 +174,7 @@ def check_local_database_dump():
     with open(os.path.abspath(os.getcwd()) + '/.sync/' + remote_database_dump_file_name) as f:
         lines = f.readlines()
         if "-- Dump completed on" not in lines[-1]:
-            sys.exit('[Error] Dump was not fully transferred')
+            sys.exit(_print(subject.ERROR, 'Dump was not fully transferred', False))
 
 #
 # CLEAN UP
@@ -185,7 +185,7 @@ def clean_up():
 
 def remove_remote_database_dump():
 
-    print('[Remote] Clean up')
+    _print(subject.REMOTE, 'Cleaning up', True)
     sftp = ssh_client.open_sftp()
     sftp.remove('/home/' + config['host']['remote']['user'] + '/' + remote_database_dump_file_name)
     sftp.remove('/home/' + config['host']['remote']['user'] + '/' + remote_database_dump_file_name + '.tar.gz')
@@ -194,6 +194,7 @@ def remove_remote_database_dump():
 def remove_temporary_data_dir():
     if os.path.exists(os.path.abspath(os.getcwd()) + '/.sync/'):
         shutil.rmtree(os.path.abspath(os.getcwd()) + '/.sync/')
+        _print(subject.LOCAL, 'Cleaning up', True)
 
 #
 # SSH UTILITY
@@ -202,7 +203,7 @@ def get_ssh_client():
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(hostname = config['host']['remote']['host'], username = config['host']['remote']['user'], password = remote_ssh_password, compress = True)
-    print('[Remote] Successfully connect to SSH client')
+    _print(subject.REMOTE, 'Successfully connect to SSH client', True)
     return ssh_client
 
 def run_ssh_command(command):
@@ -211,16 +212,42 @@ def run_ssh_command(command):
 
     err = stderr.read().decode()
     if err:
-        print('[Error]' + err)
+        sys.exit(_print(subject.ERROR, err, False))
 
     return stdout
 
 def download_status(sent,size):
     sent_mb=round(sent/1024/1024,1)
     size=round(size/1024/1024,1)
-    sys.stdout.write("[Remote] Download status: {0} MB of {1} MB sent".
+    sys.stdout.write(bcolors.YELLOW + "[REMOTE]" + bcolors.ENDC + " Status: {0} MB of {1} MB downloaded".
         format(sent_mb, size,))
     sys.stdout.write('\r')
+
+#
+# SYSTEM UTILITY
+#
+class bcolors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BLACK = '\033[90m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class subject:
+    INFO = bcolors.GREEN + '[INFO]' + bcolors.ENDC
+    LOCAL = bcolors.BLUE + '[LOCAL]' + bcolors.ENDC
+    REMOTE = bcolors.PURPLE + '[REMOTE]' + bcolors.ENDC
+    ERROR = bcolors.RED + '[ERROR]' + bcolors.ENDC
+
+def _print(header,message,do_print):
+    if do_print:
+        print(header + ' ' + message)
+    else:
+        return header + ' ' + message
 
 #
 # MAIN
