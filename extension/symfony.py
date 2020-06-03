9fa87807cca6188df2ed2604579ce9d47bd6c04f
Konrad Michalik <konrad.michalik@xima.de>
2020-06-03 10:17:02 +0200

    [Bugfix] Adjust symfony parser to new structure

--- extension/symfony.py
+++ extension/symfony.py
@@ -5,56 +5,53 @@ from subprocess import check_output
 from utility import output, system, connect, helper, mode
 
 
-def check_target_configuration():
-    if os.path.isfile(system.config['host']['target']['path']) == False:
+def check_local_configuration(client):
+    if os.path.isfile(system.config['host'][client]['path']) == False:
         sys.exit(output.message(output.get_subject().ERROR, 'Local database configuration not found', False))
 
     system.config['db'] = {}
 
-    output.message(
-        output.get_subject().TARGET,
-        'Checking database configuration',
-        True
-    )
-
-    _database_credentials = check_output(
-        helper.get_command('target', 'grep') + ' -v "^#" ' + system.config['host']['target'][
-            'path'] + ' | ' + helper.get_command('target', 'grep') + ' DATABASE_URL',
+    _db_credentials = check_output(
+        helper.get_command(client, 'grep') + ' -v "^#" ' + system.config['host'][client][
+            'path'] + ' | ' + helper.get_command(client, 'grep') + ' DATABASE_URL',
         stderr=subprocess.STDOUT,
         shell=True
     )
 
-    _target_db_config = parse_database_credentials(_database_credentials)
+    _db_config = parse_database_credentials(_db_credentials)
 
     if system.option['verbose']:
+        if client == mode.get_clients().TARGET:
+            _subject = output.get_subject().TARGET
+        else:
+            _subject = output.get_subject().ORIGIN
         output.message(
-            output.get_subject().TARGET,
-            output.get_bcolors().BLACK + helper.get_command('target', 'grep') + ' -v "^#" ' +
-            system.config['host']['target']['path'] + ' | ' + helper.get_command('target',
+            _subject,
+            output.get_bcolors().BLACK + helper.get_command(client, 'grep') + ' -v "^#" ' +
+            system.config['host'][client]['path'] + ' | ' + helper.get_command(client,
                                                                                  'grep') + ' DATABASE_URL' + output.get_bcolors().ENDC,
             True
         )
 
-    system.config['db']['target'] = _target_db_config
+    system.config['db'][client] = _db_config
 
 
-def check_origin_configuration():
-    output.message(output.get_subject().ORIGIN, 'Checking database configuration', True)
+def check_remote_configuration(client):
     stdout = mode.run_command(
-        helper.get_command('origin', 'grep') + ' -v "^#" ' + system.config['host']['origin'][
-            'path'] + ' | ' + helper.get_command('origin', 'grep') + ' DATABASE_URL',
-        mode.get_clients().ORIGIN
+        helper.get_command(client, 'grep') + ' -v "^#" ' + system.config['host'][client][
+            'path'] + ' | ' + helper.get_command(client, 'grep') + ' DATABASE_URL',
+        client
     )
-    _database_credentials = stdout.readlines()[0]
-    _origin_db_config = parse_database_credentials(_database_credentials)
-    system.config['db']['origin'] = _origin_db_config
+    _db_credentials = stdout.readlines()[0]
+    _db_config = parse_database_credentials(_db_credentials)
+    system.config['db'][client] = _db_config
 
 
-def parse_database_credentials(_database_credentials):
+def parse_database_credentials(_db_credentials):
     # DATABASE_URL=mysql://db-user:1234@db-host:3306/db-name
-    _database_credentials = re.findall(r"\/{2}(.+):(.+)@(.+):(\d+)\/(.+)", _database_credentials)[0]
+    _db_credentials = re.findall(r"\/{2}(.+):(.+)@(.+):(\d+)\/(.+)", _db_credentials)[0]
 
-    if len(_database_credentials) != 5:
+    if len(_db_credentials) != 5:
         sys.exit(
             output.message(
                 output.get_subject().ERROR,
@@ -63,12 +60,12 @@ def parse_database_credentials(_database_credentials):
             )
         )
 
-    _target_db_config = {
-        'dbname': _database_credentials[4],
-        'host': _database_credentials[2],
-        'password': _database_credentials[1],
-        'port': _database_credentials[3],
-        'user': _database_credentials[0],
+    _db_config = {
+        'dbname': _db_credentials[4],
+        'host': _db_credentials[2],
+        'password': _db_credentials[1],
+        'port': _db_credentials[3],
+        'user': _db_credentials[0],
     }
 
-    return _target_db_config
+    return _db_config
