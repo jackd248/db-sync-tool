54eacf2ca11ae584c102a84c810a1139c3946922
Konrad Michalik <konrad.michalik@xima.de>
2020-10-26 17:03:06 +0100

    [Bugfix] Missing test files

---
+++ tests/files/www1/LocalConfiguration.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'dbname' => 'db',
+                'driver' => 'mysqli',
+                'host' => 'db1',
+                'password' => 'db',
+                'port' => 3306,
+                'user' => 'db',
+            ],
+        ],
+    ],
+];
