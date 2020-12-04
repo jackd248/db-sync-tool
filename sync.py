8aba780e6ffe151a0189cf27493ae9ce84302843
Konrad Michalik <konrad.michalik@xima.de>
2020-12-04 17:00:29 +0100

    [Bugfix] Fix legacy shell script call

--- sync.py
+++ sync.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from db_sync_tool import sync
+from db_sync_tool import __main__
 
 #
 # Legacy feature to support old module structure
 #
 if __name__ == "__main__":
-    sync.Sync()
+    __main__.main()
