4f98187f5602e7b821d3d5d140aa6692d8a86ba8
Konrad Michalik <konrad.michalik@xima.de>
2023-01-26 12:11:36 +0100

    release: version 2.10.6

--- db_sync_tool/info.py
+++ db_sync_tool/info.py
@@ -1,6 +1,6 @@
 """
 Info script
 """
-__version__ = "2.10.5"
+__version__ = "2.10.6"
 __pypi_package_url__ = "https://pypi.org/pypi/db-sync-tool-kmi"
 __homepage__ = "https://github.com/jackd248/db-sync-tool"
